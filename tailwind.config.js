dc82b2470e9b68428ae64365944f1cba126f92fa
ProfCitron <jeveuxparler@profcitron.fr>
2022-11-13 14:53:12 +0100

    :wrench: Configure Tailwind file
    Customizing colors and typos

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,23 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                //sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                'be-vietnam-pro': ['Be Vietnam Pro', 'sans-serif'],
+                'poppins': ['Poppins', 'sans-serif']
             },
         },
+        colors: {
+            'rougeRuban': '#EC0B43',
+            'jauneAurore': '#FFCA2C',
+            'noirEncre': '#151515',
+            'bleuKlein': '#0740AF',
+            'bleuCiel': '#8BB7FC',
+            'jauneNaples': '#FFE6A9',
+            'rougeDragee': '#FFD4E2',
+            'blanc': '#FFFFFF'
+        }
     },
 
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+
 };
