6894cd7712dd3691fe24bdec2a107fcc6c7c5e83
ProfCitron <jeveuxparler@profcitron.fr>
2022-11-20 21:59:09 +0100

    :construction: Current bug : Uncaught SyntaxError: ambiguous indirect export: HeaderNav

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from '@inertiajs/inertia-vue3';
+import { HeaderNav } from '@/Components/ProfCitron/HeaderNav.vue';
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="bg-blanc min-h-screen">
+        <HeaderNav />
+    </div>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,14 +7,16 @@
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@700&family=Poppins:wght@400;600&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
         @vite(['resources/scss/app.scss', 'resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased p-6 bg-bleuKlein">
         @inertia
     </body>
 </html>
